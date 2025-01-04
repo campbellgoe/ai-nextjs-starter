1b1720c6f2d35999ac0e95b7e96aa448d02791b0
George Campbell <campbell.goe@gmail.com>
2025-01-04 17:06:12 +0000

    ignore prismjs type error

--- components/InitializedMDXEditor.tsx
+++ components/InitializedMDXEditor.tsx
@@ -1,4 +1,5 @@
 'use client'
+// @ts-ignore
 import { highlight as prismaHighlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
